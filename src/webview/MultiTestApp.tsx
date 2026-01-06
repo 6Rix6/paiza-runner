a1464563d0638b63468d7cf3e121fdd7d255e0ef
6Rix6 <tesimariku0428@outlook.jp>
2026-01-06 16:01:11 +0900

    ボタンのデザインを変更

--- src/webview/MultiTestApp.tsx
+++ src/webview/MultiTestApp.tsx
@@ -265,10 +265,28 @@ const MultiTestApp = () => {
           <h2>Test Cases</h2>
           <div className="flex gap-2">
             <Button onClick={addTestCase} className="add-btn">
-              Add Test Case
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+              </svg>
             </Button>
             <Button onClick={handleAddTestCasesFromAtCoder}>
-              From AtCoder
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-link-45deg"
+                viewBox="0 0 16 16"
+              >
+                <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z" />
+                <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z" />
+              </svg>
             </Button>
           </div>
         </div>
