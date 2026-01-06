dc910776a6f442cdd1afba2689adb463dc22ed63
6Rix6 <tesimariku0428@outlook.jp>
2026-01-07 01:36:07 +0900

    スクロールバーのスタイルを修正

--- src/webview/atCoderProblemIndex.tsx
+++ src/webview/atCoderProblemIndex.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/atcoder.css";
+import "./styles/scrollbar.css";
 
 import AtCoderProblemApp from "./apps/AtCoderProblemApp";
 
